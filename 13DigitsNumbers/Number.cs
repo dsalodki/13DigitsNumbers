3890f471d115c8f0d423e68c8864ccda626edd76
Dzmitry Salodki <dsalodki@gmail.com>
2023-09-11 15:05:02 +0300

    Add project files.

---
+++ 13DigitsNumbers/Number.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13DigitsNumbers
+{
+    public enum Number
+    {
+        Zero,
+        One,
+        Two,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        A,
+        B,
+        C
+    }
+}
---
+++ 13DigitsNumbers/Program.cs
@@ -0,0 +1,53 @@
+﻿using _13DigitsNumbers;
+using System.Net.WebSockets;
+
+int Beautifullity(Number[] number)
+{
+    int sum = 0;
+    for (int i = 0; i < 6; i++)
+    {
+        sum += (int)number[i];
+    }
+    return sum;
+}
+
+Number[] GetNext(Number[] number, int j = 0)
+{
+    if (j == number.Length) return null;
+    int i = j;
+
+    if (number[i] == Number.C)
+    {
+        number[i] = 0;
+        return GetNext(number, i + 1);
+    }
+    else
+    {
+        number[i] = number[i] + 1;
+        return number;
+    }
+}
+
+var number = new Number[] { Number.Zero, Number.Zero, Number.Zero, Number.Zero, Number.Zero, Number.Zero };
+var variants = new Dictionary<int, int>();
+do
+{
+    int beautifullity = Beautifullity(number);
+    if (variants.ContainsKey(beautifullity))
+    {
+        variants[beautifullity]++;
+    }
+    else
+    {
+        variants.Add(beautifullity, 1);
+    }
+    number = GetNext(number);
+} while (number != null);
+
+double count = 0;
+foreach (var v in variants.Values)
+{
+    count += (double)v*v;
+}
+
+Console.WriteLine($"Количество красивых чисел: {count * 13}");
